b3cbde575a4df544f4b9e4592aed87bd7043e502
Tusharshrivastav <ts3657@gmail.com>
2021-07-13 08:48:34 +0530

    fix

---
+++ database/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Connection = async () => {
+  try {
+    await mongoose.connect(process.env.DB, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    console.log("Database Connected Succesfully");
+  } catch (error) {
+    console.log("Error: ", error.message);
+  }
+};
+
+export default Connection;
